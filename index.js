0b2f373e1136e07799faf3a950ff7c0adb187479
Leon Fan <leonkind@gmail.com>
2021-10-28 15:18:18 -0500

    new stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {
     performance
   } = require('perf_hooks');
-
+// time complexity
 // big O = O(n)  
 function addUpTo(n){
     let total = 0;
@@ -29,4 +29,30 @@ let t3 = performance.now();
 fasterSum(1000000000)
 let t4 = performance.now();
 console.log(`Time to run fasterSum function: ${(t4 - t3) / 1000} seconds`)
+
+//space complexity
+// primitives (boolean, number, undefined, null) are constant space
+// strings are O(n) space
+
+//space complexity = O(1)
+const sum = (arr) => {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+
+//space complexity = O(n)
+const doubleArray = (arr) => {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++){
+        newArray.push(arr[i] * 2);
+    }
+    return newArray;
+}
+let superArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sum(superArray))
+console.log(doubleArray(superArray))
+
 
