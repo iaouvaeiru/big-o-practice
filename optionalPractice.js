276143eb068a17406c0dd90b09e06922bfd59926
Leon Fan <leonkind@gmail.com>
2021-11-02 15:32:19 -0500

    added more

--- optionalPractice.js
+++ optionalPractice.js
@@ -57,3 +57,22 @@ function averagePair(arr, num){
     return false
   }
 console.log(averagePair([1,2,3,11,], 7))
+
+function maxSubarraySum(arr, num){
+    // add whatever parameters you deem necessary - good luck!
+    if (arr.length < num){
+        return null;
+    }
+    let maxSum = 0;
+    for (let i = 0; i < num; i++){
+        maxSum += arr[i];
+    }
+    let tempSum = maxSum;
+    for (let i = num; i < arr.length; i++){
+        tempSum = tempSum + arr[i] - arr[i - num];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+console.log(maxSubarraySum([1,2,3,4,6], 2))
