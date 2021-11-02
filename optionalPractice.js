3c710ebd271900bb75e830a3229802ec4cea3474
Leon Fan <leonkind@gmail.com>
2021-11-01 22:43:40 -0500

    practice

--- optionalPractice.js
+++ optionalPractice.js
@@ -10,4 +10,25 @@ function areThereDuplicates(...args) {
     return false
   }
 
-console.log(areThereDuplicates(1,1,1,2))
+//console.log(areThereDuplicates(1,1,1,2))
+
+
+function averagePair(arr, num){
+    if (arr.length < num){
+        return false
+    }
+    let left = 0
+    let right = (arr.length - 1)
+    while(left < right){
+        let sum = arr[left] + arr[right]
+        if (sum === (num*2)) {
+            return true
+        } else if (sum - (num*2) > 0) {
+            right --
+        } else {
+            left ++
+        }
+    }
+    return false
+  }
+console.log(averagePair([1,2,3,11,], 7))
