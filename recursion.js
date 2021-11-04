4940307ac52b3dc2930d8eff10721ae68669980b
Leon Fan <leonkind@gmail.com>
2021-11-03 20:43:51 -0500

    comment

---
+++ recursion.js
@@ -0,0 +1,20 @@
+// powerOf function takes a number and the exponent as arguments
+function power(number, powerOf){
+    let ans = 1
+    if (powerOf === 0){
+        return ans
+    }
+    ans = number * power(number, powerOf -1)
+    return ans
+}
+
+// factorial function does n!
+function factorial(n){
+    let ans = 1;
+    // base case is 0
+    if (n === 0){
+        return 1;
+    }
+    ans = n * factorial(n - 1);
+    return ans;
+ }
