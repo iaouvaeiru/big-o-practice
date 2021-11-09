d2ff62c39d3927e911625b2f7c887d859a51b53a
Leon Fan <leonkind@gmail.com>
2021-11-09 15:26:16 -0600

    brute force string search

--- searchingAlgorithms.js
+++ searchingAlgorithms.js
@@ -26,3 +26,21 @@ function binarySearch(arr, value){
     }
     return -1
 }
+
+//brute force string matching function
+function naiveString(long, short){
+    let count = 0
+    for (let i = 0; i < long.length; i++){
+        for (let j = 0; j < short.length; j++){
+            if (short[j] !== long[i+j]){
+                break
+            }
+            if (j === short.length - 1){
+                count++
+            }
+        }
+    }
+    return count
+}
+
+console.log(naiveString("woweezoweewowee", "wow"))
