8b4346d77c33c52aef41322ca2562a2b00358366
Leon Fan <leonkind@gmail.com>
2021-12-14 17:29:37 -0600

    valid parentheses challenge

---
+++ validParenthesesStack.js
@@ -0,0 +1,25 @@
+function validParentheses(str){
+    let stack = [];
+    if((str.length%2) !== 0){
+        return false
+    } 
+    for(let i = 0; i < str.length; i++){
+        if (str[i]==='(' || str[i]==='{' || str[i]==='['){
+            stack.push(parenthesesConverter(str[i]));
+        } else if(stack.length === 0 || stack.pop() !== str[i]){
+            return false
+        }
+    }
+    return stack.length === 0
+}
+
+function parenthesesConverter(char){
+    if(char === '('){
+        return ')'
+    } else if (char === '['){
+        return ']'
+    } else if (char === '{'){
+        return '}'
+    }
+}
+console.log(validParentheses(''))
